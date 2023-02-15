e6ade5ddd90b8c0c0f05376bae3ccb46b18bd6fa
pbrehill <pbrehill@gmail.com>
2023-02-15 11:34:09 +1100

    Commit after test IV

--- year_start_fn.R
+++ year_start_fn.R
@@ -2,9 +2,16 @@ library(tidyverse)
 library(lubridate)
 
 start_date <- function(dob) {
+  if (is.na(dob)) {
+    return(NA)
+  }
   year <- as.numeric(format(dob, format="%Y"))
   school_start_year <- year + 5
   age5 <- paste0(format(dob, format="%d/%m/"), school_start_year) %>% as.Date(format = "%d/%m/%Y")
+  # Account for leap year babies
+  if (format(dob, format="%d/%m/") == "29/02/") {
+    age5 <- paste0("28/02/", school_start_year) %>% as.Date(format = "%d/%m/%Y")
+  }
   if (year >= 1945 & year <= 1951) {
     ssd <- paste0("31/12/", school_start_year) %>% as.Date(format = "%d/%m/%Y")
   } else if (year >= 1952 & year <= 1981) {
@@ -13,13 +20,21 @@ start_date <- function(dob) {
     ssd <- paste0("31/12/", school_start_year) %>% as.Date(format = "%d/%m/%Y")
   } else if (year >= 2003 & year <= 2021) {
     ssd <- paste0("30/06/", school_start_year) %>% as.Date(format = "%d/%m/%Y")
+  } else {
+    return(NA)
   }
   
   if (abs(age5 - ssd) < abs((age5  %m+% years(1)) - ssd)) {
-    (age5 - ssd)
+    result <- (age5 - ssd)
   } else {
-    (age5  %m+% years(1)) - ssd
+    result <- (age5  %m+% years(1)) - ssd
   }
+  
+  as.numeric(result, units="days")
 }
 
-start_date(as.Date("20/05/1994", format = "%d/%m/%Y"))
+start_date(as.Date(NA, format = "%Y-%m-%d"))
+
+# dobtest <- c("20/05/1994", "26/11/1995") %>% as.Date(format = "%d/%m/%Y")
+
+# print(dobtest %>% map_int(start_date))
